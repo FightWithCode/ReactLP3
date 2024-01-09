f106287e40c7960f4619178d950c1f1140511bac
FightWithCode <rahulsharmanoj@gmail.com~>
2024-01-09 15:03:32 +0530

    added list functionality

--- src/App.js
+++ src/App.js
@@ -23,11 +23,33 @@ function Form() {
     </div>
   )
 }
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 function PackingList() {
   return (
-    <div className="list">List</div>
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
   )
 }
+
+function Item({item}) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration:"line-through"} : {}}>{item.quantity} {item.description}</span>
+      <button>&times;</button>
+    </li>
+  )
+}
+
 function Stats() {
   return (
     <footer className="stats">
