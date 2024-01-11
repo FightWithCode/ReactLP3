b309cfa7a1fb83713bce8de4853d61a6ec785c34
FightWithCode <rahulsharmanoj@gmail.com>
2024-01-11 15:35:54 +0530

    added item code

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ export default function App() {
     setItems((items) => [...items, item] )
   }
 
+  function handleDeleteItem(id){
+    setItems((items) => items.filter((item) => item.id !== id))
+  }
+
   return (
     <div className="App">
       <Logo />
       <Form handleAddItems={handleAddItems} />
-      <PackingList items={items}/>
+      <PackingList items={items} handleDeleteItem={handleDeleteItem}/>
       <Stats />
     </div>
   );
@@ -49,23 +53,23 @@ function Form({handleAddItems}) {
   )
 }
 
-function PackingList({items}) {
+function PackingList({items, handleDeleteItem}) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} handleDeleteItem={handleDeleteItem}/>
         ))}
       </ul>
     </div>
   )
 }
 
-function Item({item}) {
+function Item({item, handleDeleteItem}) {
   return (
     <li>
       <span style={item.packed ? {textDecoration:"line-through"} : {}}>{item.quantity} {item.description}</span>
-      <button>&times;</button>
+      <button onClick={() => handleDeleteItem(item.id)}>&times;</button>
     </li>
   )
 }
