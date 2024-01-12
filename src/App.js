7adb4268ca5357d5a7e0429c0968d26de2a979d3
FightWithCode <rahulsharmanoj@gmail.com>
2024-01-12 09:36:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -60,14 +60,31 @@ function Form({handleAddItems}) {
 }
 
 function PackingList({items, handleDeleteItem, handlePacked}) {
+  const [sortBy, setSortBy] = useState("input")
+  let sortedItems
+  if (sortBy === "input") sortedItems = items
+  if (sortBy === "packed") sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed))
+  if (sortBy === "description") sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description))
+  // function handleSortItems(){
+  //   setSortBy(sortBy)
+  // }
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item key={item.id} item={item} handleDeleteItem={handleDeleteItem} handlePacked={handlePacked}/>
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        {/* <button onClick={() => handleSortItems(sortBy)}>Sort</button> */}
+      </div>
     </div>
+
   )
 }
 
